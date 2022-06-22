723cf8b9264b8898144f32a796c5b0a3654be7c2
Seth McCormick <seth7293@gmail.com>
2022-06-22 11:13:52 -0600

    initial commit

--- Tower.client/src/env.js
+++ Tower.client/src/env.js
@@ -1,6 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-9ptc0fv8.us.auth0.com'
+export const clientId = 'https://SethDev.com'
+export const audience = 'TWUCpgDir5QlmcwhsdBwGZOUMItsxpMD'
+
